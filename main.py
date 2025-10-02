675d86c0bf1e12035af2a41224695babd0b7b601
Elya Mirazizova <emul878@yahoo.com>
2025-10-02 23:17:26 +0300

    3 task

--- main.py
+++ main.py
@@ -5,3 +5,8 @@ print(sqares)
 #задание 2
 even_numbers={x for x in range(1,20) if x%2==0}
 print(even_numbers)
+
+#задание 3
+words = ["python", "Java", "c++", "Rust", "go"]
+new_list = [word.upper() for word in words if len(word)>3]
+print(new_list)
