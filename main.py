c1d344d32c6ebee281de1cc793ada5821b3d4cec
Elya Mirazizova <emul878@yahoo.com>
2025-10-02 23:32:20 +0300

    4 task

--- main.py
+++ main.py
@@ -1,12 +1,32 @@
 #задание 1
 sqares = [x**2 for x in range(1,11)]
-print(sqares)
+print("Задание 1",sqares)
 
 #задание 2
 even_numbers={x for x in range(1,20) if x%2==0}
-print(even_numbers)
+print("Задание 2",even_numbers)
 
 #задание 3
 words = ["python", "Java", "c++", "Rust", "go"]
 new_list = [word.upper() for word in words if len(word)>3]
-print(new_list)
+print("Задание 3",new_list)
+
+#задание 4
+class Countdown:
+    def __init__(self, n):
+        self.n = n
+        self.current = n
+    
+    def __iter__(self):
+        return self
+    
+    def __next__(self):
+        if self.current < 1:
+            raise StopIteration
+        value = self.current
+        self.current -= 1
+        return value
+    
+countdown = Countdown(5)
+for number in countdown:
+    print("Задание 4",number)
